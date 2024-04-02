42212a49931a27bb5e19e1857e12d8555bab6f9d
jabir254 <jabirkullow@gmail.com>
2024-04-02 13:59:28 +0300

    added static files

--- travel_services/app.js
+++ travel_services/app.js
@@ -2,24 +2,23 @@ const express = require("express");
 const expressHandlebars = require("express-handlebars");
 
 const app = express();
+
+// configure Handlebars view engine
+app.engine('handlebars', expressHandlebars.engine({
+ defaultLayout: 'main',
+}))
+
+app.set('view engine', 'handlebars')
+
 const port = process.env.PORT || 3000;
 
-app.engine(
-  "handlebars",
-  expressHandlebars({
-    defaultLayout: "main",
-  })
-);
-app.set("view engine", "handlebars");
+app.use(express.static(__dirname + '/public'))
 
-app.get("/", (req, res) => {
-  res.type("text/plain");
-  res.send("Hello World!");
-});
-app.get("/about", (req, res) => {
-  res.type("text/plain");
-  res.send("About Travels");
-});
+
+app.get("/", (req, res) => res.render('home'));
+
+app.get('/about', (req, res) => res.render('about'))
+
 app.use((req, res) => {
   res.type("text/plain");
   res.status(404);
