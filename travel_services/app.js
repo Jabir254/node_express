c1cddb7fa6b3f346940e917393c4852b4ea385ca
jabir254 <jabirkullow@gmail.com>
2024-04-03 12:28:53 +0300

    unit testing

--- travel_services/app.js
+++ travel_services/app.js
@@ -1,30 +1,27 @@
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
-const fortune = require('./fortune.js')
-const handlers = require('./lib/handlers.js')
-
+const fortune = require("./fortune.js");
+const handlers = require("./lib/handlers.js");
 
 const app = express();
 
 // configure Handlebars view engine
-app.engine('handlebars', expressHandlebars.engine({
- defaultLayout: 'main',
-}));
+app.engine(
+  "handlebars",
+  expressHandlebars.engine({
+    defaultLayout: "main",
+  })
+);
 
-app.set('view engine', 'handlebars');
+app.set("view engine", "handlebars");
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + "/public"));
 
-
-
-
-app.get('/', handlers.home)
-app.get('/about', handlers.about)
-app.use(handlers.notFound)
-app.use(handles.serverError)
-
-
+app.get("/", handlers.home);
+app.get("/about", handlers.about);
+app.use(handlers.notFound);
+app.use(handles.serverError);
 
 app.listen(port, () => console.log(`started on port ${port}`));
