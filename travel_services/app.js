928d1862913ca7ee247436f41ca8a554f0dc74d8
jabir254 <jabirkullow@gmail.com>
2024-04-04 11:52:07 +0300

    added singup routes to app

--- travel_services/app.js
+++ travel_services/app.js
@@ -1,31 +1,37 @@
 /* eslint-disable no-console */
 /* eslint-disable import/extensions */
-const express = require('express');
-const expressHandlebars = require('express-handlebars');
-const fortune = require('./lib/fortune.js');
-const handlers = require('./lib/handlers.js');
-const bodyParser = require('body-parser')
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const fortune = require("./lib/fortune.js");
+const handlers = require("./lib/handlers.js");
+const bodyParser = require("body-parser");
 const app = express();
 
 // configure Handlebars view engine
 app.engine(
-  'handlebars',
+  "handlebars",
   expressHandlebars.engine({
-    extname: '.hbs',
-    defaultLayout: 'main',
-  }),
+    extname: ".hbs",
+    defaultLayout: "main",
+  })
 );
 
-app.set('view engine', 'handlebars');
+app.set("view engine", "handlebars");
 
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(`${__dirname}/public`));
 
-app.get('/', handlers.home);
-app.get('/about', handlers.about);
+app.get("/", handlers.home);
+app.get("/about", handlers.about);
+
+//signup routes
+app.get("/newsletter-signup", handlers.newsletterSignup);
+app.post("/newsletter-signup/process", handlers.newsletterSignupProcess);
+app.get("/newsletter-signup/thank-you", handlers.newsletterSignupThankYou);
+
 app.use(handlers.notFound);
 app.use(handlers.serverError);
 
@@ -33,5 +39,4 @@ if (require.main === module) {
   app.listen(port, () => console.log(`started on port ${port}`));
 } else {
   module.exports = app;
-
 }
