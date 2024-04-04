9b94e70d8600e5af1f1499c05c7217b8cffc9075
jabir254 <jabirkullow@gmail.com>
2024-04-04 18:00:38 +0300

    endpoints for form submission

--- travel_services/app.js
+++ travel_services/app.js
@@ -21,6 +21,7 @@ app.set("view engine", "handlebars");
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use(express.static(`${__dirname}/public`));
 
@@ -32,6 +33,9 @@ app.get("/newsletter-signup", handlers.newsletterSignup);
 app.post("/newsletter-signup/process", handlers.newsletterSignupProcess);
 app.get("/newsletter-signup/thank-you", handlers.newsletterSignupThankYou);
 
+//endpoint for newsletter
+app.get("/newsletter", handlers.newsletter);
+app.post("/api/newsletter-signup", handlers.api.newsletterSignup);
 app.use(handlers.notFound);
 app.use(handlers.serverError);
 
